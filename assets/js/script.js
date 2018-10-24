564539491cd57b27b52632d8b8d2c2f969c827fb
nisted <clement.p10@laposte.net>
2018-10-24 11:05:20 +0200

    premier depot

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+$(function(){
+  $('#firstText').css('border', '5px green dashed');
+  $('#thirdText').css('border', '5px green dashed');
+});
+// pour afficher un border et le styliser il faut appliquer le .css
